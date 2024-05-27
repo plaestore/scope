a27cfaf0d563c2c26aedc872d7ff7ba21e08ba8d
plaestore <137597064+plaestore@users.noreply.github.com>
2024-05-27 00:40:19 -1200

    unary scope resolution operator

---
+++ main.cpp
@@ -0,0 +1,18 @@
+                              //Scope and Unary scope resolution opereator (::)//
+#include <iostream>
+
+using namespace std;
+
+
+int tuna = 45;
+
+int main()
+{
+    double tuna = 21.9;
+    cout << tuna << endl;
+    cout << ::tuna <<endl; //By adding the :: u can access the tuna variable outside the main function//
+
+}
+
+
+
